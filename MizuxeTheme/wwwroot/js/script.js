b1ab5ffb037912ccf6273c200b043a0061f2731c
Marek Kotowski <marek.kotowski87@gmail.com>
2018-07-14 22:44:47 +0200

    footer, scrollspy, smooth scrolling done

--- MizuxeTheme/wwwroot/js/script.js
+++ MizuxeTheme/wwwroot/js/script.js
@@ -1,2 +1,20 @@
 ﻿//Get the current year for copyright
 $('#year').text(new Date().getFullYear());
+
+//init scrollspy
+$('body').scrollspy({ target: "#main-nav" });
+
+//smooth scrolling
+$('#main-nav a').on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
